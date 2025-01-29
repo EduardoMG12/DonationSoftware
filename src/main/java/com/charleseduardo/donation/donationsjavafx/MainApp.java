c055ca78ecf2852e7fc3155d52273beed33becbd
charles.eduardo.mello.guimaraes@hotmail.com <charles.eduardo.mello.guimaraes@hotmail.com>
2025-01-29 14:43:02 -0300

    feat: test registe screen

--- src/main/java/com/charleseduardo/donation/donationsjavafx/MainApp.java
+++ src/main/java/com/charleseduardo/donation/donationsjavafx/MainApp.java
@@ -12,17 +12,29 @@ import java.net.URL;
 
 public class MainApp extends Application {
 
-    @Override
     public void start(Stage primaryStage) throws IOException {
-        AnchorPane loader = FXMLLoader.load(MainApp.class.getResource("home.fxml"));
+        AnchorPane loader = FXMLLoader.load(MainApp.class.getResource("register.fxml"));
         Scene scene = new Scene(loader);
 
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Donation Program - Home");
+        primaryStage.setTitle("Donation Program - Register");
         primaryStage.setResizable(true);
         primaryStage.setMaximized(true);
         primaryStage.show();
     }
+
+// Tela Home
+//    @Override
+//    public void start(Stage primaryStage) throws IOException {
+//        AnchorPane loader = FXMLLoader.load(MainApp.class.getResource("home.fxml"));
+//        Scene scene = new Scene(loader);
+//
+//        primaryStage.setScene(scene);
+//        primaryStage.setTitle("Donation Program - Home");
+//        primaryStage.setResizable(true);
+//        primaryStage.setMaximized(true);
+//        primaryStage.show();
+//    }
 
     public static void main(String[] args) {
         launch(args);
